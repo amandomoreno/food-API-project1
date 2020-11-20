89c349c854041f599cb3f0d29ec8e905abed70f0
amandomoreno <amandomoreno47@gmail.com>
2020-11-20 15:15:06 -0600

    advice function works

--- js/app.js
+++ js/app.js
@@ -69,22 +69,26 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+const allImages = []
 const biryaniImg = []
 const burgerImg = []
 const dosaImg = []
 const idlyImg = []
 const pizzaImg = []
-const advise = []
+const advices = []
 
 
 /*------------------------ Cached Element References ------------------------*/
 const randBtn = document.getElementById('randomizer')
 const adviseBtn = document.getElementById('adviceButton');
+const container = document.getElementById("containerDiv")
 const body = document.getElementById('body')
+const slip = document.getElementById("adviceSlip")
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-randBtn.addEventListener('click', ()=> {//how to randomize 5 categories being fetched?
+randBtn.addEventListener('click', ()=> {
     fetch("https://foodish-api.herokuapp.com/api/images/biryani")
     .then((response) => {
         console.log(response)
@@ -168,10 +172,10 @@ adviseBtn.addEventListener('click', ()=> {
         return response.json()
     })
     .then((data) => {
-                let newAdvise = {}
-                newAdvise["artist"] = "unknown"
-                newAdvise["advise"] = data.advise
-                advise.push(newAdvise)
+                let newAdvice = {}
+                newAdvice["artist"] = "unknown"
+                newAdvice["advise"] = data.slip.advice
+                advices.push(newAdvice)
                 render();
     })
     .catch((err) => {
@@ -179,118 +183,38 @@ adviseBtn.addEventListener('click', ()=> {
     });
 })    
 /*-------------------------------- Functions --------------------------------*/
-// function biryaniCat() {
-//     fetch("https://foodish-api.herokuapp.com/api/images/biryani")
-//     .then((response) => {
-//         console.log(response)
-//         return response.json()
-//     })
-//     .then((data) => {
-//         console.log(data)
-//                 // let newImage = {}
-//                 // newImage["cat"] = "biryani"
-//                 // newImage["image"] = data.image
-//                 // biryaniImg.push(newImage)
-//                 // render();
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//      });
-// };
+//function for second APi
+function appendDiv(advice, artist, idx) {
+    let newDiv = document.createElement("div")
+    newDiv.innerHTML = `
+                        <div class="card h-100" id="${artist.toLowerCase()}">
+                            <div class="card-body">
+                                <blockquote class="blockquote mb-0">
+                                    <p>${advice}</p>
+                                    <footer class="blockquote-footer">${artist}</footer>
+                                </blockquote>
+                            </div>
+                            <button id="delButton" class="btn" onClick={deleteAdvice(${idx})}>X</button>
+                        </div>    
+                        `
+    slip.appendChild(newDiv)
+}
 
-// function burgerCat() {
-//     fetch("https://foodish-api.herokuapp.com/api/images/burger")
-//     .then((response) => {
-//         console.log(response)
-//         return response.json()
+// function render() {
+//     container.innerHTML = ""
+//     allImages.forEach((cat, idx) => {
+//         appendDiv(cat["image"], cat["cat"], idx)
 //     })
-//     .then((data) => {
-//                 let newImage = {}
-//                 newImage["cat"] = "burger"
-//                 newImage["image"] = data.image
-//                 burgerImg.push(newImage)
-//                 render();
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//      });    
-// };
-
-// function dosaCat() {
-//     fetch("https://foodish-api.herokuapp.com/api/images/dosa")
-//     .then((response) => {
-//         console.log(response)
-//         return response.json()
-//     })
-//     .then((data) => {
-//                 let newImage = {}
-//                 newImage["cat"] = "dosa"
-//                 newImage["image"] = data.image
-//                 dosaImg.push(newImage)
-//                 render();
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//      });     
-// };
-
-// function idlyCat() {
-//     fetch("https://foodish-api.herokuapp.com/api/images/idly")
-//     .then((response) => {
-//         console.log(response)
-//         return response.json()
-//     })
-//     .then((data) => {
-//                 let newImage = {}
-//                 newImage["cat"] = "idly"
-//                 newImage["image"] = data.image
-//                 idlyImg.push(newImage)
-//                 render();
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//      });     
-// };
-
-// function pizzaCat() {
-//     fetch("https://foodish-api.herokuapp.com/api/images/pizza")
-//     .then((response) => {
-//         console.log(response)
-//         return response.json()
-//     })
-//     .then((data) => {
-//                 let newImage = {}
-//                 newImage["cat"] = "pizza"
-//                 newImage["image"] = data.image
-//                 pizzaImg.push(newImage)
-//                 render();
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//      });     
 // };
 
 function render() {
-    container.innerHTML = ""
-    categories.forEach((cat, idx) => {
-        appendDiv(cat["image"], cat["cat"], idx)
+    slip.innerHTML = ""
+    advices.forEach((advice, idx) => {
+        appendDiv(advice["advise"], advice["artist"], idx)
     })
 }
 
-//function for second APi
-
-// function appendDiv(advice, artist) {
-//     let newDiv = document.createElement("div")
-//     newDiv.innerHTML = `
-//                         <div class="card h-100" id="${artist.toLowerCase()}">
-//                             <div class="card-body">
-//                                 <blockquote class="blockquote mb-0">
-//                                     <p>${advice}</p>
-//                                     <footer class="blockquote-footer">${artist}</footer>
-//                                 </blockquote>
-//                             </div>
-//                         </div>    
-//                         `
-//     container.appendChild(newDiv)
-// }
-
+function deleteAdvise(idx){
+    advises.splice(idx, 1)
+    render()
+}
