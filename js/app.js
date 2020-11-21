4c7881b562bdbeef4723f34eda7e6612c8356e46
amandomoreno <amandomoreno47@gmail.com>
2020-11-20 23:23:34 -0600

    appendImg

--- js/app.js
+++ js/app.js
@@ -282,7 +282,7 @@ adviseBtn.addEventListener('click', ()=> {
     })
     .then((data) => {
                 let newAdvice = {}
-                newAdvice["artist"] = "unknown"
+                newAdvice["artist"] = "GOD"
                 newAdvice["advise"] = data.slip.advice
                 advices.push(newAdvice)
                 render();
@@ -296,17 +296,33 @@ adviseBtn.addEventListener('click', ()=> {
 function appendImg(image, cat) {
     let newContainer = document.createElement("div")
     newContainer.innerHTML = `
-                        <div class="card h-100" id="${cat.toLowerCase()}">
+                                <div class="card mb-3" id="${cat.toLowerCase()}" style="max-width: 540px;">
+                                    <div class="row no-gutters">
+                                        <div class="col-md-4">
+                                            <img src="${image}" class="card-img" alt="randomizer-picks">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                    <h5 class="card-title">the Randomizer picks...</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            `
+    container.appendChild(newContainer)
+}
+
+
+{/* <div class="card h-100" id="${cat.toLowerCase()}">
                             <div class="card-body">
                                 <blockquote class="blockquote mb-0">
                                      <img id="pick" class="getPick" src="${image}" alt="randomizer-picks">
                                      <footer class="blockquote-footer">${cat}</footer>
                                 </blockquote>
                             </div>
-                            </div>    
-                            `
-    container.appendChild(newContainer)
-}
+                            </div>  */}
 
 //function for second APi
 function appendDiv(advice, artist) {
